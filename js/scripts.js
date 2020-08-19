32eca001d495893b9ff3774313a7cb7bcee28c39
Lauren <TechnologicalType@MacBook-Air-2.attlocal.net>
2020-08-18 20:35:05 -0400

    focused on the contact portion - not finished

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$("#email").click(function(){
+    var elem = $("#myElem");
+    setTimeout(function(){
+        elem.removeClass("style") ;       
+    }, 1000);
+});
+
+
+
+$("#num").click(function(){
+    var elem = $("#Elem");
+    setTimeout(function(){
+        elem.removeClass("style") ;       
+    }, 1000);
+});
